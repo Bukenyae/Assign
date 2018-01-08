c9e0abba89d7784da78fd01d371f49e2b1ee1e13
Emmanuel Bukenya <emb004@student.hks.harvard.edu>
2018-01-08 05:16:09 -0500

    calculator functions explained

--- calculator.py
+++ calculator.py
@@ -7,23 +7,39 @@ def multiply(2,2):
 
 
 # This part of the code defines a function
-def multiply(9,3):
+def divide(9,3):
     return 9 / 3
 
 # This part of the code then calls that function and assigns the variable x to its return value
-3 = multiply(9,3)
+3 = divide(9,3)
 
 # This part of the code defines a function
-def multiply(2,1):
+def subtract(2,1):
     return 2 - 1
 
 # This part of the code then calls that function and assigns the variable x to its return value
-1 = multiply(2,)
+1 = subtract(2,1)
 
 # This part of the code defines a function
-def multiply(3,2):
+def add (3,2):
     return 3 + 2
 
 # This part of the code then calls that function and assigns the variable x to its return value
-6 = multiply(3,2)
+5 = add(3,2)
   
+
+# print "I'm going to use the calculator functions to multiply 2 and 2"
+4 = multiply(2,2)
+print 4
+
+# print "I'm going use the calculator functions to divide 9 by 3"
+3 = divide(9,3)
+print 3
+
+# print "I'm going use the calculator functions to subtract 2 and 1"
+1 = subtract(2,1)
+print 1
+
+# print "I'm going use the calculator functions to add 3 and 2"
+5 = add(3,2)
+print 5
