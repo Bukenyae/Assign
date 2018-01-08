0ef52bde4fe398783929362fd5880da57bb27abd
Emmanuel Bukenya <emb004@student.hks.harvard.edu>
2018-01-08 04:32:45 -0500

    added say hello

---
+++ sayhello.py
@@ -0,0 +1,11 @@
+Assign
+#!/usr/bin/env python2
+# this is a sample comment
+# one of the best things ever
+# can't wait to make the most of this learning process
+# pretty good lesson learnt today
+import sys
+name = sys.stdin.read()
+print "Hello " + name + "!"print "Hello, I'm a python program"
+
+
