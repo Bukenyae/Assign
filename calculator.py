65de4207a95fde4926075f587814d909ef38463d
Emmanuel Bukenya <emb004@student.hks.harvard.edu>
2018-01-08 05:02:11 -0500

    added calculator functions

---
+++ calculator.py
@@ -0,0 +1,29 @@
+# This part of the code defines a function
+def multiply(2,2):
+    return 2 * 2
+
+# This part of the code then calls that function and assigns the variable x to its return value
+4 = multiply(2,2)
+
+
+# This part of the code defines a function
+def multiply(9,3):
+    return 9 / 3
+
+# This part of the code then calls that function and assigns the variable x to its return value
+3 = multiply(9,3)
+
+# This part of the code defines a function
+def multiply(2,1):
+    return 2 - 1
+
+# This part of the code then calls that function and assigns the variable x to its return value
+1 = multiply(2,)
+
+# This part of the code defines a function
+def multiply(3,2):
+    return 3 + 2
+
+# This part of the code then calls that function and assigns the variable x to its return value
+6 = multiply(3,2)
+  
